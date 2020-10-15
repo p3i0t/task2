981d08ea26953c206b4b8e5762dd24a05df66c9d
a-name-not-used <wangxin_0716@me.com>
2020-10-15 13:14:13 +0800

    x

--- models.py
+++ models.py
@@ -27,12 +27,12 @@ class BasicBlock(nn.Module):
 class ResFeatureNet(nn.Module):
     def __init__(self):
         super().__init__()
-
-        self.res1 = BasicBlock(1, 32, stride=2)
-        self.res2 = BasicBlock(32, 64, stride=2)
-        self.res3 = BasicBlock(64, 128, stride=2)
-        self.res4 = BasicBlock(128, 256, stride=2)
-        self.res5 = BasicBlock(256, 512, stride=2)
+        self.conv = nn.Conv2d(1, 32, kernel_size=7, stride=1, padding=3),
+        self.res1 = BasicBlock(32, 64, stride=2)
+        self.res2 = BasicBlock(64, 128, stride=2)
+        self.res3 = BasicBlock(128, 256, stride=2)
+        self.res4 = BasicBlock(256, 512, stride=2)
+        self.res4 = BasicBlock(512, 1024, stride=2)
         self.flatten = nn.Flatten()
 
     def forward(self, x):
